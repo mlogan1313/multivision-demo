a54aa733748c5186d9aaaeddd6237cf68b26eb09
Matt Logan <mattlogan@Jeejos-MBP-3.lan>
2015-10-23 16:18:40 -0500

    End lesson 4

--- server.js
+++ server.js
@@ -32,27 +32,17 @@ if (env === 'development') {
 	mongoose.connect('mongodb://mlogan1313:multivision@ds043324.mongolab.com:43324/multivision');	
 }
 
-
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Connection error...'));
 db.once('open', function callback() {
 	console.log('multivision db opened');
 });
-var messageSchema = mongoose.Schema({ message: String });
-var Message = mongoose.model('Message', messageSchema);
-var mongoMessage;
-Message.findOne().exec(function (err, messageDoc) {
-	mongoMessage = messageDoc.message;
-})
-
 
 app.get('/partials/:partialPath', function (req, res) {
 	res.render('partials/' + req.params.partialPath);
 });
 app.get('*', function (req, res) {
-	res.render('index', {
-		mongoMessage: mongoMessage
-	});
+	res.render('index');
 });
 
 var port = process.env.PORT || 3030;
