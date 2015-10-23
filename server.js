cb53da252e073fbf030e18fafa2d071da584af68
Matt Logan <mattlogan@Jeejos-MBP-3.lan>
2015-10-23 14:59:16 -0500

    Walking skeleton

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express'),
 	stylus = require('stylus'),
 	logger = require('morgan'),
-	bodyParser = require('body-parser');
+	bodyParser = require('body-parser'),
+	mongoose = require('mongoose');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -14,7 +15,7 @@ function compile(str, path) {
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
 app.use(logger('dev'));
-// app.use(bodyParser());
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(stylus.middleware(
@@ -25,11 +26,27 @@ app.use(stylus.middleware(
 ));
 app.use(express.static(__dirname + '/public'));
 
+mongoose.connect('mongodb://localhost/multivision');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error...'));
+db.once('open', function callback() {
+	console.log('multivision db opened');
+});
+var messageSchema = mongoose.Schema({ message: String });
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.findOne().exec(function (err, messageDoc) {
+	mongoMessage = messageDoc.message;
+})
+
+
 app.get('/partials/:partialPath', function (req, res) {
 	res.render('partials/' + req.params.partialPath);
 });
 app.get('*', function (req, res) {
-	res.render('index');
+	res.render('index', {
+		mongoMessage: mongoMessage
+	});
 });
 
 var port = 3030;
